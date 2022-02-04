8a83c3a942022c4e31f124b268aae4f8f52bc85f
kallyas <kallyasmedia@gmail.com>
2022-02-04 03:39:59 +0300

    ch: create Hero section

---
+++ components/Hero.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="center-image">
+        <img alt="" className="logo-img" src="./img/zuri-logo-full.svg" />
+      </div>
+
+      <div className="hero">
+        <h2>Learn, Build, Grow.</h2>
+        <p>
+          Unlock your Brilliance with our hands-on <a href="https://enrollment-v3.vercel.app/">beginner</a>{" "}
+          and <a href="https://outbox.co.ug/">expert training</a>. Outbox Edu Team has been
+          immensely successful in creating a global network of a highly adept intelligent workforce
+          that can help your company achieve their mission-critical 
+          <a href="https://outbox.co.ug/"> projects and goals</a>
+        </p>
+        <div className="btn-div">
+          <a href="https://enrollment-v3.vercel.app/">
+            <button className=" my-btn btn-red">I'm new to the Industry</button>
+          </a>
+          <br className="d-block d-md-none" />
+          <a href="https://outbox.co.ug/">
+            <button className=" my-btn btn-red-outline">I need industry experience</button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
